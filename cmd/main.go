fe508fa4d5eb001cc0aa682dc32c4d640115ae83
dmitriyrazgulyaev <dimaraz2006@gmail.com>
2024-12-22 16:31:44 +0300

    server part created

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,8 @@
-package cmd
+package main
+
+import "github.com/DmitriyRazgulyaev/calc_go/internal/application"
+
+func main() {
+	app := application.New()
+	app.RunServer()
+}
--- internal/application/application.go
+++ internal/application/application.go
@@ -1,1 +1,62 @@
 package application
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/DmitriyRazgulyaev/calc_go/pkg/calculation"
+	"net/http"
+	"os"
+)
+
+type Config struct {
+	Addr string
+}
+
+func ConfigFromEnv() *Config {
+	config := new(Config)
+	config.Addr = os.Getenv("PORT")
+	if config.Addr == "" {
+		config.Addr = "8080"
+	}
+	return config
+}
+
+type Application struct {
+	config *Config
+}
+
+func New() *Application {
+	return &Application{
+		config: ConfigFromEnv(),
+	}
+}
+
+type Request struct {
+	Expression string `json:"expression"`
+}
+
+func CalcHandler(w http.ResponseWriter, r *http.Request) {
+	request := new(Request)
+
+	defer r.Body.Close()
+	err := json.NewDecoder(r.Body).Decode(request)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	result, err := calculation.Calc(request.Expression)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+
+	}
+
+	fmt.Fprintf(w, "result: %f", result)
+}
+
+func (a *Application) RunServer() error {
+	http.HandleFunc("/api/v1/calculate/", CalcHandler)
+	return http.ListenAndServe(":"+a.config.Addr, nil)
+}
